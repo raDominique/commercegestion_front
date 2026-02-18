8a09c36ae9d1bf31bdf4a99f2faeb1a209672ca8
Scrat1313 <patrickramahefarson08022002@gmail.com>
2026-02-19 00:27:42 +0300

    feat: add next-themes dependency and update Toaster component usage

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 
 import AppRoutes from './routes/AppRoutes.jsx';
-import ToasterProvider from './components/ui/ToasterProvider.jsx';
+import { Toaster } from './components/ui/sonner';
 
 function App() {
   const apiUrl = import.meta.env.VITE_API_BASE_URL;
   console.log('API URL:', apiUrl);
   return (
     <>
-      <ToasterProvider />
+      <Toaster position="top-right" />
       <AppRoutes />
     </>
   );
